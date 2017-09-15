43613ea37442ca36b275418aaf43cd9f1e0e761d
jaco <jaco.com>
2017-09-15 16:05:40 +1000

    wip

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
+  console.log(req.url)
   res.send('Hello World!')
+})
+
+app.get('/favicon.ico', function(req, res) {
+    console.log('fave')
 })
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
+
+app.use(express.static('public'))
